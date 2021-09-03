f28a39e2c5fc1f4368ae04dfa3e4add0c2c70b5b
catherineruoxiwu <ruoxi.wu@uwaterloo.ca>
2021-09-03 22:29:30 +0800

    Halfway done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
     { id: 'e1', title: 'Car Insurance', amount: '26.79', date: new Date(2021, 2, 28) },
     { id: 'e2', title: 'Car Insurance', amount: '26.79', date: new Date(2021, 2, 28) }
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,30 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import React, { useState } from "react";
 
 function Expenses(props) {
   const item1 = props.expenses[0];
   const item2 = props.expenses[1];
+  const [year, setYear] = useState("2020");
+
+  const yearChangeHandler = (year) => {
+    setYear(year);
+  }
   return (
     <Card className="expenses">
-      <ExpenseItem title={item1.title} amount={item1.amount} date={item1.date} />
-      <ExpenseItem title={item2.title} amount={item2.amount} date={item2.date} />      
+      <ExpensesFilter selected={year} onYearChange={yearChangeHandler}/>
+      <ExpenseItem
+        title={item1.title}
+        amount={item1.amount}
+        date={item1.date}
+      />
+      <ExpenseItem
+        title={item2.title}
+        amount={item2.amount}
+        date={item2.date}
+      />
     </Card>
   );
 }
